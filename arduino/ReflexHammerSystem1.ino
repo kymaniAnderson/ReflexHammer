f911bdb3dd4f54e514a381d127b3d8d1a16d28f5
kymaniAnderson <53957063+kymaniAnderson@users.noreply.github.com>
2020-05-27 13:03:23 -0500

    Update ReflexHammerSystem1.ino
    This takes a new approach to finding displacement and displaying data. Displacement is calibrated using a variable resistor attached to a gear in which is turned by a string attached to the subject's leg.

--- arduino/ReflexHammerSystem1.ino
+++ arduino/ReflexHammerSystem1.ino
@@ -1,86 +1,78 @@
-#include <MPU6050_tockn.h>
 #include <Wire.h>
-#include <LiquidCrystal.h>
- 
-MPU6050 mpu6050(Wire);
+#include "ds3231.h"
 
-const int RS = 2,
-          EN = 3,
-          D4 = 4,
-          D5 = 5,
-          D6 = 6,
-          D7 = 7;
-
-LiquidCrystal lcd(RS, EN, D4, D5, D6, D7);
-
-double displacement; 
-
-int forceSnsrPin = 15;
-int forceSnsrReading;
-
-unsigned long forceSnsrResistance;
-unsigned long forceSnsrConductance;
-
+int displacement;
 long forceSnsrForce;
+float emgVoltage;
 
 void setup()
 {
-  lcd.begin(16, 2);
- 
+  Serial.begin(9600);
   Wire.begin();
  
-  mpu6050.begin();
-  mpu6050.calcGyroOffsets(true);
-  Serial.begin(9600);
-}
+  DS3231_init(DS3231_CONTROL_INTCN);
+
+  tableHead();
+ }
 
 void loop()
 {
-    mpu6050.update();
-    
-    getEMG();
-    //getAngle();
-    //getDisplacement();
-    getImpactForce();
+  getImpactForce();
+  getEMG();
+  getDisplacement();
+  tableBody();
 
-    delay(50);
-    lcd.clear();
+  delay(350);
 }
 
-void getAngle()
+void tableHead()
 {
-  //    Serial.println("=======================================================");
-  //    Serial.print("accX : ");Serial.print(mpu6050.getAccX());
-  //    Serial.print("  accY : ");Serial.print(mpu6050.getAccY());
-  //    Serial.print("  accZ : ");Serial.println(mpu6050.getAccZ());
- 
-  //    Serial.print("accAngleX : ");Serial.print(mpu6050.getAccAngleX());
-  //    Serial.print("  accAngleY : ");Serial.println(mpu6050.getAccAngleY());
-  
-  //    Serial.print("angleX : ");Serial.print(mpu6050.getAngleX());
-  //    Serial.print("  angleY : ");Serial.print(mpu6050.getAngleY());
-  //    Serial.print("  angleZ : ");Serial.println(mpu6050.getAngleZ());
-  //    Serial.println("=======================================================");
+  Serial.print("Time (HH/MM/SS)");
+  Serial.print("\t\t");
+  Serial.print("EMG Voltage (V)");
+  Serial.print("\t\t");
+  Serial.print("Impact (N)");
+  Serial.print("\t");
+  Serial.print("Displacement (cm)");
+  Serial.println();
+}
 
-      lcd.setCursor(0, 0);
-      lcd.print("Y:");
-      lcd.print(mpu6050.getAccY());
+void tableBody()
+{
+  struct ts t; 
+  DS3231_get(&t);
   
-      lcd.setCursor(8, 0);
-      lcd.print("Z:");
-      lcd.print(mpu6050.getAccZ());
+  //Time (HH/MM/SS)
+  Serial.print(t.hour);
+  Serial.print(":");
+  Serial.print(t.min); 
+  Serial.print(":");
+  Serial.print(t.sec);
+  Serial.print("\t\t\t");
+  
+  //Voltage (V)
+  Serial.print(emgVoltage);
+  Serial.print("\t\t\t");
+  
+  //Impact (N)
+  Serial.print(forceSnsrForce);
+  Serial.print("\t\t");
+   
+  //Displacement (cm)
+  Serial.print(displacement); //manually taken
+  Serial.println();
 }
 
 void getImpactForce()
 {
-  forceSnsrReading  = analogRead(forceSnsrPin);
+  int forceSnsrReading  = analogRead(A0);
   int forceSnsrVoltage = map(forceSnsrReading, 0, 1023, 0, 5000);
   
-  forceSnsrResistance = 5000 - forceSnsrVoltage;     
+  unsigned long forceSnsrResistance = 5000 - forceSnsrVoltage;     
   forceSnsrResistance *= 10000;                
   forceSnsrResistance /= forceSnsrVoltage;
 
-  forceSnsrConductance = 1000000;           
+  unsigned long forceSnsrConductance = 1000000;           
   forceSnsrConductance /= forceSnsrResistance;
 
   if (forceSnsrConductance <= 1000) 
@@ -91,55 +83,16 @@ void getImpactForce()
    {
       forceSnsrForce = (forceSnsrConductance - 1000) / 30;          
    }
-    
-  Serial.print("Force: ");
-  Serial.println(forceSnsrForce);
-  Serial.println("N");
-  
-  lcd.setCursor(0, 1);
-  lcd.print("Impact: ");
-  lcd.print(forceSnsrForce);
-  lcd.print("N");
-}
-
-void getDisplacement()
-{
-   double initY = mpu6050.getAngleY();
-   double initZ = mpu6050.getAngleZ();
-
-   double tempY = initY + 2;
-   double tempZ = initZ + 2;
-
-   double finY; 
-   double finZ; 
-    
-   while (initY > tempY && initZ > tempZ)//never enters this loop.. why?
-   {//while movement detected:
-     Serial.println("==================================");
-     Serial.println("MOVEMENT!");
-     Serial.println("==================================");
-     finY = mpu6050.getAngleY();
-     finZ = mpu6050.getAngleZ();
-    }
-    
-   double temp = sq(finY - initY) + sq(finZ - initY);
-   displacement = sqrt(temp);
-   Serial.print("Displacement: ");
-   Serial.println(displacement);
-   Serial.println("----------------------------");
-   lcd.setCursor(0, 1);
-   lcd.print("Dis: ");
-   lcd.print(displacement);
-   lcd.print("m");
 }
 
 void getEMG()
 {
   float emgReading = analogRead(A1);
-  float voltage = emgReading * (5.0 / 1023.0);
+  emgVoltage = emgReading * (5.0 / 1023.0);
+}
 
-  Serial.println("==================================");
-  Serial.print("EMG Voltage: ");
-  Serial.println(voltage);
-  Serial.println("==================================");
+void getDisplacement()
+{
+  int dispReading = analogRead(A2);
+  displacement = map(dispReading,0,1023,5,35);
 }
